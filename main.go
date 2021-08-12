f2d4d87a323e6cfa2599154444ea976474bf13a8
Fawaz Farid <fawwazally@gmail.com>
2021-08-12 17:59:56 +0300

    Add log line to handler

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+        log.Println("This is a Golang log line, you're better off using Node logs anyway")
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello AWS Lambda and Netlify",
